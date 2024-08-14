54a8ddcdf06b6edfe4f737acf12893000420aefc
rs <rs@qq.com>
2024-08-14 22:24:23 +0800

    完成字符串的hash树

--- test/test_main.c
+++ test/test_main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+#include "hash.h"
+#include "tools.h"
 
 int main()
 {
+    hash_char_t *hash_char = NULL;
+    hash_char = add_hash_char(hash_char, "aa");
+    hash_char = add_hash_char(hash_char, "bb");
+    hash_char = add_hash_char(hash_char, "cc");
+
+    hash_char = del_hash_char(hash_char, "bb");
+
+    free_hash_char(hash_char);
 
     return 0;
 }
--- tools/hash.c
+++ tools/hash.c
@@ -1,2 +1,125 @@
 #include "hash.h"
 
+// hash_char_t* malloc_hash_char() {
+//     hash_char_t* ret = (hash_char_t*)malloc(sizeof(hash_char_t));
+//     if (ret == NULL) {
+//         printf("malloc_hash_char error\n");
+//         return NULL;
+//     }
+
+//     ret->left = NULL;
+//     ret->right = NULL;
+//     ret->key = (char*)malloc(sizeof(char));
+//     *(ret->key) = '\0';
+
+//     return ret;
+// }
+
+void free_hash_char(hash_char_t *hash_char) {
+    // 后序遍历，删除所有节点
+    if (hash_char == NULL) {
+        return;
+    }
+
+    free_hash_char(hash_char->left);
+    free_hash_char(hash_char->right);
+
+    free(hash_char->key);
+    free(hash_char);    
+}
+
+hash_char_t* add_hash_char(hash_char_t *hash_char, const char *str) {
+    if (hash_char == NULL) {
+        // 没有这个节点，则添加
+        char *p_str = (char*)malloc(sizeof(char) * (strlen(str) + 1));
+        strcpy(p_str, str);
+
+        hash_char_t *node = (hash_char_t*)malloc(sizeof(hash_char_t));
+        node->key = p_str;
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+    }
+
+    int k = strcmp(hash_char->key, str);
+    if (k == 0) {
+        // 存在相同节点，直接返回当前节点
+        return hash_char;
+    } else if (k > 0) {
+        // 左边
+        hash_char->left = add_hash_char(hash_char->left, str);
+        return hash_char;
+    } else {
+        // 右边
+        hash_char->right = add_hash_char(hash_char->right, str);
+        return hash_char;
+    }
+}
+
+ret_code_t find_hash_char(const hash_char_t *hash_char, const char* str) {
+    // 先序查找
+    if (hash_char == NULL) {
+        return ERROR;
+    }
+
+    int k = strcmp(hash_char->key, str);
+    if (k == 0)
+    {
+        return SUCCESS;
+    } else if (k > 0) {
+        // 左边
+        return find_hash_char(hash_char->left, str);
+    } else {
+        // 右边
+        return find_hash_char(hash_char->right, str);
+    }
+}
+
+hash_char_t* del_hash_char(hash_char_t *hash_char, const char* str) {
+    if (hash_char == NULL) {
+        return NULL;
+    }
+
+    int k = strcmp(hash_char->key, str);
+    if (k == 0) {
+        // 查找到相同字符，需要删掉
+        if (hash_char->left == NULL && hash_char->right == NULL) {
+            // 叶子节点
+            free(hash_char->key);
+            free(hash_char);
+            return NULL;
+        } else if (hash_char->left == NULL) {
+            // 仅有右节点
+            hash_char_t *tmp = hash_char->right;
+            free(hash_char->key);
+            free(hash_char);
+            return tmp;
+        } else if (hash_char->right == NULL) {
+            // 仅有左节点
+            hash_char_t *tmp = hash_char->left;
+            free(hash_char->key);
+            free(hash_char);
+            return tmp;
+        } else {
+            // 左右节点均存在
+            hash_char_t *right = hash_char->right;
+            hash_char_t *p = right;
+            while (p->left != NULL) {
+                p = p->left;
+            }
+            // 节点转移
+            p->left = hash_char->left;
+            free(hash_char->key);
+            free(hash_char);
+            return right;
+        }
+    } else if (k > 0) {
+        // 左边
+        hash_char->left = del_hash_char(hash_char->left, str);
+        return hash_char;
+    } else {
+        // 右边
+        hash_char->right = del_hash_char(hash_char->right, str);
+        return hash_char;
+    }
+}
