8dc7549fecf92f1efe902a16abc33f952095a07a
rs <rs@qq.com>
2024-08-27 01:08:04 +0800

    dev: 改变

--- src/simple/simple.h
+++ src/simple/simple.h
@@ -60,4 +60,9 @@ int largestPerimeter(int* nums, int numsSize);
 int* sortedSquares(int* nums, int numsSize, int* returnSize);
 int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
 
+int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
+int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
+int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
+int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
+
 #endif
