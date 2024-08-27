c0673bf442ef73adb4ff4941ef27ef677918305d
rs <rs@qq.com>
2024-08-27 21:41:06 +0800

    ok

--- src/simple/simple.h
+++ src/simple/simple.h
@@ -58,7 +58,6 @@ int repeatedNTimes(int* nums, int numsSize);
 bool isUnivalTree(struct TreeNode* root);
 int largestPerimeter(int* nums, int numsSize);
 int* sortedSquares(int* nums, int numsSize, int* returnSize);
-
 int* addToArrayForm(int* num, int numSize, int k, int* returnSize);
 
 #endif
