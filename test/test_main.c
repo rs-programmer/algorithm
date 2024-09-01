521886aa8d631c80050b9c5bf406eb8e7fdcc9f8
rs <rs@qq.com>
2024-09-01 21:04:09 +0800

    merge dev

--- test/test_main.c
+++ test/test_main.c
@@ -1,7 +1,15 @@
-#include "simple_1.h"
+#include "simple.h"
+#include "hash.h"
+
+#include "sort.h"
 
 int main()
 {
+    int nums[] = {0};
+    int k = 1000;
+    int size = 0;
     
+    addToArrayForm(nums, 1, k, &size);
     return 0;
 }
+
--- tools/search.h
+++
@@ -1,7 +0,0 @@
-#ifndef search_h
-#define search_h
-
-int search_binary(int *nums, int numsSize, int target);
-
-
-#endif
